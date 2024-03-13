dafe110508c4d536cc3821099ce927503019c8d4
lmsand <lmsand99@icloud.com>
2024-03-13 00:25:39 -0500

    Insert/log food

---
+++ src/components/FoodLogListItem.tsx
@@ -0,0 +1,28 @@
+import { StyleSheet, Text, View } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+
+
+const FoodLogListItem = ({ item }) => {
+  return (
+    <View style={styles.container}>
+      <View style={{ flex: 1, gap: 5 }}>
+        <Text style={{ fontWeight: "bold", fontSize: 16 }}>{item.label}</Text>
+        <Text style={{ color: "dimgray" }}>{item.kcal} cal</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#f6f6f8",
+    padding: 10,
+    gap: 5,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderRadius: 5,
+  },
+});
+
+export default FoodLogListItem;
